3be67112bb18a16974a10f317ddaa426b21f8f92
Patrick James <patrick.james@my.sfcollege.edu>
2024-09-11 20:38:44 -0400

    Lesson 2.4 - Collision Decisions

--- Unit_2-Basic Gameplay/Prototype_2/Assets/Script/PlayerController.cs
+++ Unit_2-Basic Gameplay/Prototype_2/Assets/Script/PlayerController.cs
@@ -7,24 +7,33 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 20.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //prevents player from moving out of bounds
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
-        {            
+        {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput =Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
