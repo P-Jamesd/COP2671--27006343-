6c892552f410de1f8fbbe1d85cfe5df763345105
Patrick James <patrick.james@my.sfcollege.edu>
2024-09-22 21:12:11 -0400

    complete 4.26

---
+++ Unit_4-Gameplay Mechanics/Prototype_4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefabs;
+    private float spawnRange = 9.0f;
+    void Start()
+    {
+        Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosz = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX,0, spawnPosz);
+        return randomPos;
+    }
+}
